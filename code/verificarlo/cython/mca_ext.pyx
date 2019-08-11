c8e4cc56f187f9d757d7ae7c97914956e724a32b
gkiar <gkiar07@gmail.com>
2019-08-11 16:16:45 -0400

    minor reorg

--- code/verificarlo/cython/mca_ext.pyx
+++
@@ -1,6 +0,0 @@
-def myf(int x):
-    cdef double result
-    result = 1.4
-    for i in range(x):
-        result += 0.1
-    print(result)
--- code/verificarlo/hello-world/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-  double a = 0;
-  for (int i=0; i < 10000; i++) a += 0.1;
-  printf("%0.17f\n", a);
-  return 0;
-}
