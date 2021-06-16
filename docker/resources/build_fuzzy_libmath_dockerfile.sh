28ac4de5dd2610f98feced1fc932259ad9059b06
yohanchatelain <yohan.chatelain@gmail.com>
2021-06-16 11:10:16 -0400

    Add detailled help for each arguments and optional argument for the fuzzy image to use

--- docker/resources/build_fuzzy_libmath_dockerfile.sh
+++ docker/resources/build_fuzzy_libmath_dockerfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOCKERFILE=Dockerfile.mcalibmath
+FUZZY_IMAGE_DEFAULT=verificarlo/fuzzy:v0.4.1-lapack
 
 generate_docker() {
     
@@ -10,8 +11,8 @@ FROM ${1}
 
 # Copy fuzzy environment from fuzzy image
 RUN mkdir -p /opt/mca-libmath
-COPY --from=verificarlo/fuzzy:v0.4.1-lapack /opt/mca-libmath/libmath.so /opt/mca-libmath/
-COPY --from=verificarlo/fuzzy:v0.4.1-lapack /usr/local/lib/libinterflop* /usr/local/lib/
+COPY --from=${2} /opt/mca-libmath/libmath.so /opt/mca-libmath/
+COPY --from=${2} /usr/local/lib/libinterflop* /usr/local/lib/
 
 # Set environment variables
 ENV VFC_BACKENDS 'libinterflop_mca.so --precision-binary32=24 --precision-binary64=53 --mode=mca'
@@ -24,15 +25,26 @@ build_docker() {
     docker build -f ${DOCKERFILE} -t ${1} .
 }
 
-if [[ $# < 2 ]]; then
-    echo "usage: ./build_fuzzy_libmath_dockerfile <DOCKER_IMAGE> <TAG>"
+if [[ $# < 2 ]]
+then
+    echo "usage: ./build_fuzzy_libmath_dockerfile.sh <DOCKER_IMAGE> <TAG> [FUZZY_IMAGE]"
+    echo "          <DOCKER_IMAGE>: Name of the base docker image to build"
+    echo "          <TAG>:          Tag of the new image to build"
+    echo "          [FUZZY_IMAGE]:  Name of the fuzzy image to copy from (optional)"
     exit 1
-else
-    BASEIMAGE=$1
+elif [[ $# == 2 ]]
+then
+    BASE_IMAGE=$1
     TAG=$2
+    FUZZY_IMAGE=$FUZZY_IMAGE_DEFAULT
+elif [[ $# == 3 ]]
+then
+    BASE_IMAGE=$1
+    TAG=$2
+    FUZZY_IMAGE=$3
 fi
 
-echo "Build fuzzy-libmath for ${BASEIMAGE} as ${TAG}"
-generate_docker $BASEIMAGE
+echo "Build fuzzy-libmath (from ${FUZZY_IMAGE}) for ${BASEIMAGE} as ${TAG}"
+generate_docker $BASEIMAGE $FUZZY_IMAGE
 build_docker $TAG
 
